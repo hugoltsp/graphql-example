e90c6dfe1105333aff023ba7dbe7cf10f61dadbf
hugoltsp <hugo-pires@outlook.com>
2018-05-31 23:57:15 -0300

    JDK9+ compability

--- src/main/java/com/teles/graphql/config/GraphQLConfig.java
+++ src/main/java/com/teles/graphql/config/GraphQLConfig.java
@@ -1,23 +1,23 @@
 package com.teles.graphql.config;
 
-import java.io.IOException;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.Resource;
-
 import com.teles.graphql.fetcher.CharacterByFavoriteBeerFetcher;
 import com.teles.graphql.fetcher.CharacterByNameFetcher;
 import com.teles.graphql.fetcher.CharacterByUniverseFetcher;
 import com.teles.graphql.fetcher.CharacterListFetcher;
-
 import graphql.GraphQL;
 import graphql.schema.GraphQLSchema;
 import graphql.schema.idl.RuntimeWiring;
 import graphql.schema.idl.SchemaGenerator;
 import graphql.schema.idl.SchemaParser;
 import graphql.schema.idl.TypeRuntimeWiring.Builder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 @Configuration
 public class GraphQLConfig {
@@ -33,7 +33,7 @@ public class GraphQLConfig {
 	private final CharacterListFetcher listFetcher;
 
 	public GraphQLConfig(@Value("classpath:characters.graphql") Resource resource,
-			CharacterByUniverseFetcher byUniverseFetcher, 
+			CharacterByUniverseFetcher byUniverseFetcher,
 			CharacterByNameFetcher byNameFetcher,
 			CharacterByFavoriteBeerFetcher byFavoriteBeerFetcher,
 			CharacterListFetcher listFetcher) {
@@ -44,12 +44,14 @@ public class GraphQLConfig {
 		this.listFetcher = listFetcher;
 	}
 
-	@Bean
-	public GraphQL buildGraphQL() throws IOException {
-		RuntimeWiring runtimeWiring = RuntimeWiring.newRuntimeWiring().type("Query", this::apply).build();
-		GraphQLSchema graphQLSchema = new SchemaGenerator().makeExecutableSchema(new SchemaParser().parse(resource.getFile()), runtimeWiring);
-		return GraphQL.newGraphQL(graphQLSchema).build();
-	}
+    @Bean
+    public GraphQL buildGraphQL() throws IOException {
+        RuntimeWiring runtimeWiring = RuntimeWiring.newRuntimeWiring().type("Query", this::apply).build();
+        GraphQLSchema graphQLSchema = new SchemaGenerator()
+                .makeExecutableSchema(new SchemaParser()
+                        .parse(new BufferedReader(new InputStreamReader(resource.getInputStream()))), runtimeWiring);
+        return GraphQL.newGraphQL(graphQLSchema).build();
+    }
 
 	private Builder apply(Builder typeWiring) {
 		return typeWiring.dataFetcher("byName", byNameFetcher)
